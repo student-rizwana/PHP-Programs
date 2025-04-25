7441aff537fd03333a093a698a02b168580da21c
Rizwana Dange <rizwanadange242@gmail.com>
2025-04-25 09:44:13 +0530

    Add files via upload

---
+++ mathematical_functions..php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php
+  // 1. Initialize any variable and display ceiling value of 443.3432
+  $num1 = 443.3432;
+  echo "Ceiling value of $num1 is: " . ceil($num1) . "<br>";
+
+  // 2. Initialize any variable and display floor value of 7543.829
+  $num2 = 7543.829;
+  echo "Floor value of $num2 is: " . floor($num2) . "<br>";
+
+  // 3. Display any random number between 500 to 5000
+  $random = rand(500, 5000);
+  echo "Random number between 500 and 5000: " . $random . "<br>";
+  ?>
+
+</body>
+
+</html>
